e2725e9fdcaadf69d1503e3f9cdf6594d9a2159a
Babystando581 <Babystando581@gmail.com>
2024-09-26 23:09:30 +0100

    help me

---
+++ Other/sort.rs
@@ -0,0 +1,14 @@
+
+fn sort(mut arr){
+    for i in 0..(arr.len()-1){
+        if arr[i+1]>=arr[i]{
+        }
+    }
+}
+
+
+
+fn main(){
+    let mut my_array=[55,5, 2, 17, 11, 45, 1, 23];
+    sort(my_array)
+}
