30c6b69012d2522aa9f830388148a0c3e67d4385
d.visone <d.visone@london-oratory.org>
2024-09-27 11:49:01 +0100

    fixed some of the array typing

--- Other/sort.rs
+++ Other/sort.rs
@@ -1,7 +1,10 @@
-
-fn sort(mut arr){
+fn sort(arr: &mut [i32]){
     for i in 0..(arr.len()-1){
         if arr[i+1]>=arr[i]{
+            let temp = arr[i+1];
+            arr[i+1] = arr[i];
+            arr[i] = temp;
+            println!("{:#?}",arr);
         }
     }
 }
@@ -9,6 +12,6 @@ fn sort(mut arr){
 
 
 fn main(){
-    let mut my_array=[55,5, 2, 17, 11, 45, 1, 23];
-    sort(my_array)
+    let mut my_array:[i32;8]=[55,5, 2, 17, 11, 45, 1, 23];
+    sort(my_array &mut [i32]);
 }
