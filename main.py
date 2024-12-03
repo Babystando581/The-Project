ddfe9fd409bd45e7241018d2b73f7ce979f0e611
d.visone <d.visone@london-oratory.org>
2024-12-03 14:59:20 +0000

    Rene has faced the depths of Hell and returned intact, but never the same

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 import pygame
 import sys
+import random
 from controls import controls
+
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Celeste clone")
-        self.screen = pygame.display.set_mode((640, 480))
+        self.screen = pygame.display.set_mode(size := (640, 480))
 
         self.clock = pygame.time.Clock()  # eeeeeeee
 
         self.img = pygame.image.load('data/images/sample2.png')
         self.img.set_colorkey((0, 0, 0))
 
-        self.img_pos = [160, 260]
+        self.hitbox_pos = [160, 260]
+
+        self.hitbox = self.img.get_rect()
 
         self.x_movement = [False, False]
 
         self.y_movement = [False, False]
 
+        self.background = pygame.Surface(size)
+
     def run(self):
+        backgroundcolour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         while True:
-            self.screen.fill(14)
-            self.img_pos[0] += (self.x_movement[1] - self.x_movement[0]) * 6
-            self.img_pos[1] += (self.y_movement[1] - self.y_movement[0]) * 6
-            self.screen.blit(self.img, self.img_pos)
+            self.screen.fill(backgroundcolour)
+            self.hitbox_pos[0] += (self.x_movement[1] - self.x_movement[0]) * 6
+            self.hitbox_pos[1] += (self.y_movement[1] - self.y_movement[0]) * 6
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == controls('up'):
                         self.y_movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == controls('down'):
                         self.y_movement[1] = True
-                    if event.key == pygame.K_LEFT:
+                    if event.key == controls('left'):
                         self.x_movement[0] = True
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == controls('right'):
                         self.x_movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == controls('up'):
                         self.y_movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == controls('down'):
                         self.y_movement[1] = False
-                    if event.key == pygame.K_LEFT:
+                    if event.key == controls('left'):
                         self.x_movement[0] = False
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == controls('right'):
                         self.x_movement[1] = False
 
+            self.hitbox.topleft = self.hitbox_pos
+            self.hitbox.clamp_ip(self.background.get_rect())
+            self.hitbox_pos = list(self.hitbox.topleft)
+            self.screen.blit(self.img, self.hitbox_pos)
             pygame.display.update()
             self.clock.tick(60)
-            self.img = pygame.image.load('data/images/sample2.png')
 
 
 Game().run()
