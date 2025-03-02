b3df781268450e78e68ae789c934c445e903b70c
Babystando581 <Babystando581@gmail.com>
2025-03-02 21:50:05 +0000

    pain

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from controls import mapper, setter, bodge
 from gravity import air_time, jumping_bodge
 
-size = [0, 0]
+size = [1280, 720]
 
 
 class Block:
@@ -16,19 +16,24 @@ class Block:
             self.dimensions = dimensions
             self.surf = pygame.Surface(self.dimensions)
             if colour:
-                self.surf.fill(colour)
+                self.colour = colour
+                self.surf.fill(self.colour)
             else:
+                self.colour = (255, 255, 255)
                 self.surf.blit(
                     pygame.image.load('data/images/source_mod.png').subsurface(pygame.Rect((0, 0), self.dimensions)),
                     (0, 0))
 
         self.hitbox = pygame.Rect(coords, dimensions)
 
-        if not colour:
-            self.colour = (255, 255, 255)
 
+solid_list = []
 
-floor = Block((0, 720 - 30), (1280, 30), None, (255, 255, 255))
+floor = Block((0, size[1] - 30), (size[0], 30), None, (255, 255, 255))
+solid_list.append(floor)
+
+test_platform = Block((1000, 500), (200, 50), None, (100, 100, 100))
+solid_list.append(test_platform)
 
 
 class Game:
@@ -36,7 +41,7 @@ class Game:
         global size
         pygame.init()
         pygame.display.set_caption("Celeste clone")
-        self.screen = pygame.display.set_mode(size := (1280, 720))
+        self.screen = pygame.display.set_mode(size)
 
         self.clock = pygame.time.Clock()
 
@@ -66,20 +71,25 @@ class Game:
         timer = 0
 
         while True:
+            print(self.hitbox.bottom)
             timer += 0.2
             self.x_speed = (self.x_movement[1] - self.x_movement[0]) * 6
-            if pygame.Rect.colliderect(self.hitbox, floor.hitbox) is True:
-                self.y_speed = 0
-            else:
-                self.y_speed = ((self.y_movement[1] - self.y_movement[0]) * 15) + (0.8 * (air_time(timer))) ** 1.8
+            for i in range(len(solid_list)):
+                if pygame.Rect.colliderect(self.hitbox, solid_list[i].hitbox) is True:
+                    self.y_speed = 0
+                else:
+                    self.y_speed = ((self.y_movement[1] - self.y_movement[0]) * 15) + (0.8 * (air_time(timer))) ** 1.8
             self.screen.fill(backgroundcolour)
             self.hitbox.move_ip(self.x_speed, self.y_speed)
-            if pygame.Rect.colliderect(self.hitbox, floor.hitbox) is True:
+            if pygame.Rect.colliderect(self.hitbox, solid_list[0].hitbox) is True:
                 jumping_bodge(True)
-                self.hitbox.bottom = size[1] - floor.dimensions[1]
+                self.hitbox.bottom = size[1] - solid_list[0].dimensions[1]
                 self.y_movement[0] = False
             else:
                 jumping_bodge(False)
+
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -105,6 +115,8 @@ class Game:
                         self.hitbox.width *= 1.2
                         self.hitbox.height *= 1.2
                         self.hitbox.bottom = size[1] - floor.dimensions[1]
+                    if event.key == pygame.K_t:
+                        self.hitbox.bottom = 300
                 if event.type == pygame.KEYUP:
                     # print('Up:', event.key)
                     if event.key == mapper('up') and pygame.Rect.colliderect(self.hitbox, floor.hitbox) is True:
@@ -116,10 +128,13 @@ class Game:
                     if event.key == mapper('right'):
                         self.x_movement[1] = False
             self.hitbox.clamp_ip(self.background.get_rect())
-            self.screen.blit(floor.surf, floor.coords)
+            for i in range(len(solid_list)):
+                self.screen.blit(solid_list[i].surf, solid_list[i].coords)
             self.screen.blit(self.img, self.hitbox.topleft)
             pygame.display.update()
             self.clock.tick(60)
 
+
+print(type(solid_list[0]))
 
 Game().run()
